de6e40a18ae853f47ad9054d1769ca4062f51628
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2024-01-16 18:01:09 +0530

    PlayWatchListButtonOnFrontEnd

--- app/components/RecentlyAdded.tsx
+++ app/components/RecentlyAdded.tsx
@@ -1,6 +1,7 @@
 
 import Image from "next/image";
 import prisma from "../utils/db";
+import { MovieCard } from "./MovieCard";
 
 
 async function getData (){
@@ -12,7 +13,11 @@ async function getData (){
             title: true,
             WatchLists: true,
             imageString: true,
-            videoSource: true,
+            youtubeString: true,
+            age :true,
+            release:true,
+            duration:true,
+
         },
         orderBy: {
             createdAt: "desc",
@@ -35,7 +40,10 @@ export default async function RecentlyAdded (){
 
                         <Image src={movie.imageString} alt="Movie " width={500} height={400} className="rounded-sm absolute w-full h-full object-cover" />
                         <div className="h-60 relative z-10 w-full tranform duration-500 hover:scale-125 opacity-0 hover:opacity-100">
-                            <Image src={movie.imageString} alt="Movie" width={800} height={800} className="absolute w-full h-full -z-10 rounded-lg object-cover"></Image>
+                            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center border">
+                                <Image src={movie.imageString} alt="Movie" width={800} height={800} className="absolute w-full h-full -z-10 rounded-lg object-cover"></Image>
+                                <MovieCard movieId={movie.id} overview={movie.overview} title={movie.title} watchListId={movie.WatchLists[0]?.id} youtubeUrl={movie.youtubeString} watchList={movie.WatchLists.length>0?true:false} key={movie.id} age={movie.age} time={movie.duration} year={movie.release}/>
+                            </div>
                         </div>
                     </div>
                 ))
