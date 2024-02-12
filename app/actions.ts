4f52029f954240fb5a612f7d0346df5dbce392c6
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2024-02-12 19:02:36 +0530

    AddedAddToWatchList

--- app/actions.ts
+++ app/actions.ts
@@ -1,2 +1,21 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import prisma from "./utils/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./utils/auth";
+
+export async function addTowatchlist(formData: FormData){
+    "use server"
+
+    const movieId = formData.get("movieId");
+    const pathname = formData.get("pathname") as string;
+    
+    const data = await prisma.watchList.create({
+        data:{
+            id: Number(movieId),
+        },
+    });
+
+    revalidatePath(pathname);
+}
