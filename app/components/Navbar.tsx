79eb5f01446952c1441747725421ac69ccc3b733
krishsuthar1504 <131298127+krishsuthar1504@users.noreply.github.com>
2024-02-15 16:25:33 +0530

    LogoLinkUpdated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,10 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
-import Logo from '../../public/netflix_logo.svg'
+import Logo from '../../public/MovieInsight_logo.png'
 import { usePathname } from "next/navigation";
 import { Bell, Search } from "lucide-react";
 import UserNav from "./UserNav";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { revalidatePath } from "next/cache";
 
 interface linkProps{
     name:string;
@@ -27,9 +24,9 @@ export default function Navbar({handlenavigation}:{handlenavigation : any}){
     }
     return (
         <div className="w-full max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
-            <div className="flex items-center">
-                <Link href="/home" className="w-32">
-                    <Image src={Logo} alt="Netflix logo" priority />
+            <div className="flex items-center justify-center">
+                <Link href="/home" className="w-40">
+                    <Image src={Logo} alt="MovieInsight logo" priority />
                 </Link>
                 <ul className="lg:flex gap-x-4 ml-14 hidden">
                     {links.map((link,idx)=>(
